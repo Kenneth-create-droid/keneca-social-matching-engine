f2653cc128988bc8397073f1d6acdc7918f145d9
Kenneth Nnah <ken1@Kenneths-MacBook-Air.local>
2026-08-03 19:21:00 -0400

    We worked on getting all cells in a specific ring

---
+++ keneca-engine/src/propagation.js
@@ -0,0 +1,49 @@
+// for this propagation the job of the propagation is the create the search space for my user's so basically what cells in what ring am i trying to find 
+
+const h3_lib = require("h3-js");
+
+function cellsAtCurrentRing(startingCell, current_Ring){
+
+    // ring should not be manually encoded 
+
+    if (current_Ring == 0){
+
+        // let us return an array as the number of ceels as every ring will have this format
+        // we just want thereturn statement to be the same as all types of return statement
+
+        return [startingCell];
+
+    }
+
+    else{
+        // we use the grid Ring function from the h3 library to get all all cells in that specific ring and we store them in an array
+
+    
+
+        // now we retun all the cells in that  ring
+
+        // this retunrn the array
+
+        return h3_lib.gridRing(startingCell, current_Ring);
+
+
+    }
+        
+
+    
+
+};
+
+// this is our test function that we will use 
+const test = cellsAtCurrentRing(
+    "892b986616bffff",
+    1
+);
+
+
+
+
+
+
+
+
